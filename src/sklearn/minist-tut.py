96481319c316fde8835fff7432c5ecd8ce97e7d2
Sameh Kamal <sameh.kamaleldin@gmail.com>
2016-04-02 10:42:20 +0100

    add classification report to minsit

--- src/sklearn/minist-tut.py
+++ src/sklearn/minist-tut.py
@@ -6,7 +6,7 @@
 # Reference   | http://scikit-learn.org/stable/tutorial/basic/tutorial.html
 #-------------------------------------------------------------------------------
 
-from sklearn import datasets, svm
+from sklearn import datasets, svm, metrics
 from numpy   import array
 
 # load minsit dataset
@@ -23,17 +23,19 @@ minsit_test_set         = data[ dt_count/2 : -1]
 minsit_test_set_target  = minsit.target[ dt_count/2 : -1 ]
 
 # Create a classifier: a support vector machine classifier
-svm_model_obj = svm.SVC(gamma=0.001)
+svm_classifier_obj = svm.SVC(gamma=0.001)
 
 # train the svm model using the training set
-svm_model_obj.fit( minsit_train_set, minsit_train_set_target )
+svm_classifier_obj.fit( minsit_train_set, minsit_train_set_target )
 
 # predict the testing set
+predicted = svm_classifier_obj.predict( minsit_test_set )
 expected  = minsit_test_set_target
-predicted = svm_model_obj.predict( minsit_test_set )
 
 # calculate errors array (1 for error 0 otherwise)
 errors    = array([ [1,0][x==y] for x, y in zip(expected, predicted)])
 
+# print svm classifier parameters
+print("classifier: %s \n\n" % svm_classifier_obj)
 # print model accuracy 1 - error percentage
-print("# accuracy: %2.3f %%" % ( (1 - sum(errors)/len(errors)) * 100 ) )
+print("%s" % metrics.classification_report(expected, predicted) )
