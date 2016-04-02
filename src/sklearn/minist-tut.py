375bd9422a7a2b5a87f85fcbb7fa263d3ceaa41e
Sameh Kamal <sameh.kamaleldin@gmail.com>
2016-04-02 11:01:04 +0100

    add logestic regression tut for minsit

--- src/sklearn/minist-tut.py
+++ src/sklearn/minist-tut.py
@@ -6,7 +6,7 @@
 # Reference   | http://scikit-learn.org/stable/tutorial/basic/tutorial.html
 #-------------------------------------------------------------------------------
 
-from sklearn import datasets, svm, metrics
+from sklearn import datasets, svm, linear_model, metrics
 from numpy   import array
 
 # load minsit dataset
@@ -21,21 +21,46 @@ minsit_train_set_target = minsit.target[ 1 : dt_count/2 ]
 # define the testing set
 minsit_test_set         = data[ dt_count/2 : -1]
 minsit_test_set_target  = minsit.target[ dt_count/2 : -1 ]
+expected                = minsit_test_set_target
 
-# Create a classifier: a support vector machine classifier
-svm_classifier_obj = svm.SVC(gamma=0.001)
+# ------------------------------------------------------------------------------
+# Support Vector Machine
+# ------------------------------------------------------------------------------
+
+# initialize support vector machine classifier
+svm_classifier_obj = svm.SVC()
 
 # train the svm model using the training set
 svm_classifier_obj.fit( minsit_train_set, minsit_train_set_target )
 
-# predict the testing set
-predicted = svm_classifier_obj.predict( minsit_test_set )
-expected  = minsit_test_set_target
-
-# calculate errors array (1 for error 0 otherwise)
-errors    = array([ [1,0][x==y] for x, y in zip(expected, predicted)])
+# predict the testing set using svm model
+predicted_svm = svm_classifier_obj.predict( minsit_test_set )
 
 # print svm classifier parameters
-print("classifier: %s \n\n" % svm_classifier_obj)
-# print model accuracy 1 - error percentage
-print("%s" % metrics.classification_report(expected, predicted) )
+print("------------------------------------------------------------------\n")
+print("classifier: %s \n" % svm_classifier_obj)
+print("------------------------------------------------------------------\n")
+# print classification model report
+print("%s" % metrics.classification_report(expected, predicted_svm) )
+print("------------------------------------------------------------------\n")
+
+# ------------------------------------------------------------------------------
+# Logistic Regression
+# ------------------------------------------------------------------------------
+
+# initialize logistic regression classifier object
+logreg_classifer_obj = linear_model.LogisticRegression()
+
+# train the logistic regression classifier on minsit training set
+logreg_classifer_obj.fit( minsit_train_set,minsit_train_set_target )
+
+# predict the testing set using logistic regression model
+predicted_logreg = logreg_classifer_obj.predict( minsit_test_set )
+
+# print logestic regression classifier parameters
+print("------------------------------------------------------------------\n")
+print("classifier: %s \n" % logreg_classifer_obj)
+print("------------------------------------------------------------------\n")
+# print classification model report
+print("%s" % metrics.classification_report(expected, predicted_logreg) )
+print("------------------------------------------------------------------\n")
